a288741af2d1cd110b218dff5e0d035e7328fb17
Mathias Danielsen <mathiasda98@gmail.com>
2021-03-09 16:29:28 +0100

    Display allergens on the main restaurant page and the menu item page

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,9 +1,29 @@
 class MenuItem < ApplicationRecord
   has_many :order_items
-  has_many :allergen_item
+  has_many :allergen_items
   before_destroy :ensure_not_referenced_by_any_order_item
   belongs_to :menu
   has_one_attached :image
+
+  def get_allergen_names_short 
+    allergens_arr = Array.new
+
+    allergen_items.each do |allergen|
+      allergens_arr << allergen.allergen.short_name
+    end
+
+    return allergens_arr
+  end
+
+  def get_allergen_names_long 
+    allergens_arr = Array.new
+
+    allergen_items.each do |allergen|
+      allergens_arr << allergen.allergen.name
+    end
+
+    return allergens_arr
+  end
 
   private
 
