9e166ceecdf89105c98664af6f43a185ba67e03c
Nicole Bendu <nic.bendu@gmail.com>
2021-03-10 15:07:23 +0100

    orders:design med slett-knapp

--- app/javascript/components/order_item.jsx
+++ app/javascript/components/order_item.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {useState} from 'react';
 import PropTypes from 'prop-types';
+import {useSpring, animated} from 'react-spring'
 
 const orderItem = ({menuItem, quantity}) => {
-
 
     orderItem.propTypes = {
         name: PropTypes.string,
@@ -11,12 +11,24 @@ const orderItem = ({menuItem, quantity}) => {
         price: PropTypes.number
     }
 
-
+    const [zero, setZero] = useState(false)
     const [item, setItem] = useState(menuItem);
     const [name, setName] = useState(menuItem.name);
     const [num, setNum] = useState(quantity);
     const [price, setPrice] = useState(parseInt(menuItem.price));
     const [totalPrice, setTotalPrice] = useState(parseInt(menuItem.price * quantity));
+
+
+
+    const slide = useSpring({
+        marginLeft: zero ? -80 : 0,
+        marginRight: zero ? 80 : 0,
+        from: {
+            marginLeft: 0,
+            marginRight: 0
+            }
+        });
+
 
     const total_price = (val) => {
         setTotalPrice(price * val);
@@ -25,14 +37,15 @@ const orderItem = ({menuItem, quantity}) => {
     const incNum = () => {
         setNum(num + 1);
         total_price(num + 1)
+
     }
 
     const decNum = () => {
-        if (num > 0) {
+        if (num > 1) {
             setNum(num - 1);
             total_price(num - 1)
         } else {
-            setNum(0);
+            setZero(true)
         }
     }
 
@@ -41,30 +54,34 @@ const orderItem = ({menuItem, quantity}) => {
             ...item,
             quantity: event.target.value
         })
-
     }
 
     return (
-        <div className="m-1 flex justify-between pt-4">
-            <div className="flex justify-between">
-                <div className="text-gray-800 ">
-                    <div className="flex text-sm">
-                        <button onClick={decNum} onChange={handleChange}>
-                            &mdash;
-                        </button>
-                        <p className="px-3"> {num} </p>
-                        <button onClick={incNum} onChange={handleChange}>
-                            &#xff0b;
-                        </button>
+        <div className="relative ">
+            <animated.div style={slide} className="px-8 py-1.5 my-0.5 flex bg-white justify-between z-10 relative" >
+                <div className="flex justify-between">
+                    <div className="text-gray-800 ">
+                        <div className="flex ">
+                            <button onClick={decNum} onChange={handleChange} className="text-green-400 ">
+                                &mdash;
+                            </button>
+                            <p className="px-3"> {num} </p>
+                            <button onClick={incNum} onChange={handleChange} className="text-green-400 ">
+                                &#xff0b;
+                            </button>
+                        </div>
+                    </div>
+                    <div className="pl-7 text-gray-500" onClick={() => setZero(!zero)}>
+                        {name}
                     </div>
                 </div>
-                <div className="pl-7 text-gray-500">
-                    {name}
+                <div className="text-gray-700 ">
+                    {totalPrice},00 NOK
                 </div>
-            </div>
-            <div className="text-gray-700 ">
-                {totalPrice},00 kr
-            </div>
+            </animated.div>
+            <button id="delete" className="w-20 bg-green-300 py-1.5 absolute top-0 right-0 ">
+                Slett
+            </button>
         </div>
     )
 }
