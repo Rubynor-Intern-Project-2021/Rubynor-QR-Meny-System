13a115c30fe0202ff5d6f020cefa03e1cec39e01
jbg92 <jbg92@live.no>
2021-03-15 10:08:03 +0100

    commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,22 +3,20 @@ class Order < ApplicationRecord
   belongs_to :restaurant
 
   enum order_status: {
-    "Bestilt" => 0,
-    "Ferdig" => 1
+    "Ferdig" => 0,
+    "Betalt" => 1
   }
 
   def add_menu_item(menu_item)
-    current_item = order_items.find_by(menu_item_id: menu_item.id)
-    if current_item
-      current_item.quantity += 1
-    else
-      current_item = order_items.build(menu_item_id: menu_item.id)
-    end
+
+    current_item = order_items.build(menu_item_id: menu_item["item_id"])
+    current_item.quantity = menu_item["amount"]
+
     current_item
   end
 
   def total_price
-    order_items.to_a.sum { |item| item.total_price }
+    order_items.to_a.sum{|item|item.total_price}
   end
 
 end
