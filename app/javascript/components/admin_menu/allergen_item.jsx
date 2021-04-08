5cfcb9ec12c291357c773f70edd2c1fa52b1c161
jbg92 <jbg92@live.no>
2021-04-08 12:45:00 +0200

    Skjuling og sletting av menyer, Sletting av alergener

--- app/javascript/components/admin_menu/allergen_item.jsx
+++ app/javascript/components/admin_menu/allergen_item.jsx
@@ -28,13 +28,13 @@ const AllergenItem = ({allergen}) => {
         <tr className="admin-tbl-border">
             <th className="text-left">
                 <div className="pl-2 md:pl-8 lg:pl-8 inline-block ">
-                    {allergen.short_name} - {allergen.name} 
+                    {allergen.short_name} - {allergen.name}
                 </div>
                 <div className="inline-block">
                 </div>
                 <div className="flex inline-block float-right pr-4 md:pr-8 lg:pr-8">
                     <Link icon={<BiPencil size={20}/>} label="Rediger" href={Routes.edit_admin_allergen_path(allergen)}/>
-                    <Link icon={<BiXCircle size={20}/>} label="Slett" href={Routes.admin_allergen_path(allergen)}/>
+                    <Link icon={<BiXCircle size={20}/>} label="Slett" href={Routes.admin_allergen_slett_path({a_id: allergen.id})}/>
                 </div>
             </th>
         </tr>
