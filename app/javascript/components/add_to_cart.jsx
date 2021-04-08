a2984042eced5faa05151572a546de595ec8aa17
nicole bendu <nic.bendu@gmail.com>
2021-04-08 15:27:24 +0200

    Lagt til popup på menu_item sida

--- app/javascript/components/add_to_cart.jsx
+++ app/javascript/components/add_to_cart.jsx
@@ -1,43 +1,40 @@
-import React, {useReducer} from 'react'
+import React, {useEffect} from 'react'
 import {useState} from 'react';
 import PropTypes from 'prop-types';
 import axios from "axios";
 import {store} from './configureStore'
 import {AiOutlinePlus, AiOutlineMinus} from "react-icons/ai";
+import {ModalAlert} from "./modal";
 
 const addToCart = ({menuItem}) => {
-
-
     addToCart.propTypes = {
         price: PropTypes.number
     }
 
-    const [item, setItem] = useState(menuItem);
     const [num, setNum] = useState(1);
-    const [price, setPrice] = useState(parseInt(menuItem.price));
-    const [totalPrice, setTotalPrice] = useState(parseInt(menuItem.price * 0));
+    const [openModal, setOpenModal] = useState(false);
 
+    const handleOpen = () => {
+        setOpenModal(true);
+    };
 
-    const total_price = (val) => {
-        setTotalPrice(price * val);
-    }
+    const handleClose = () => {
+        setOpenModal(false);
+    };
 
     const incNum = () => {
         setNum(num + 1);
-        total_price(num + 1)
     }
 
     const decNum = () => {
         if (num > 0) {
             setNum(num - 1);
-            total_price(num - 1)
         } else {
             setNum(0);
         }
     }
-    
-    function addMultipleToCart() {
-        console.log("TEEST"+menuItem.id)
+
+    const addMultipleToCart = () => {
         axios.get('/api/v1/add_to_cart',
             {
             params: {menu_item_id: menuItem.id, amount: num}})
@@ -48,34 +45,57 @@ const addToCart = ({menuItem}) => {
                         type: 'UPDATE_ICON',
                         payload: res.data
                     });
-                }); 
-
-                setNum(0)
+                });
+                setNum(1)
             }).catch(error => {
             console.log(error);
         })
+    };
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setOpenModal(false);
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, [openModal]);
+
+    let modal = null;
+    if(openModal) {
+        modal =
+            <div className="flex justify-end">
+                <ModalAlert
+                    text="Varen ble lagt til i handlevogna"
+                    handleClose={handleClose}
+                />
+            </div>
     }
 
     return (
-        <div className="flex justify-between py-2">
-            <div className="flex justify-between py-2">
-                <div className="text-gray-800 ">
-                    <div className="flex text-sm">
-                        <button onClick={decNum}  className="">
-                            {<AiOutlineMinus/>}
-                        </button>
-                        <p className="px-4 font-bold"> {num} </p>
-                        <button onClick={incNum}  className="">
-                            {<AiOutlinePlus/>}
-                        </button>
+        <>
+            <div className="h-12 ">
+                {modal}
+            </div>
+           <div className="flex justify-between py-2">
+                <div className="flex justify-between py-2">
+                    <div className="text-gray-800 ">
+                        <div className="flex text-sm">
+                            <button onClick={decNum}  className="">
+                                {<AiOutlineMinus/>}
+                            </button>
+                            <p className="px-4 font-bold"> {num} </p>
+                            <button onClick={incNum}  className="">
+                                {<AiOutlinePlus/>}
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div >
-                <button onClick={addMultipleToCart} className="text-xs text-white bg-gray-700 uppercase bg-white px-10 py-2">Legg i kurv</button>
+                <div >
+                    <button onClick={() => { handleOpen(); addMultipleToCart(); }}
+                            className="text-sm text-white bg-gray-700 uppercase bg-white px-10 py-3">Legg i handlekurv</button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
