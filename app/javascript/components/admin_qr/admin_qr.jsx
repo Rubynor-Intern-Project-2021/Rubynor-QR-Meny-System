15c445a910eb051f005d7050b81643042a73e49b
mathias234 <mathiasda98@gmail.com>
2021-04-15 14:19:20 +0200

    Ryddet opp i koden til admin_qr.jsx

--- app/javascript/components/admin_qr/admin_qr.jsx
+++ app/javascript/components/admin_qr/admin_qr.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import QRCode from 'easyqrcodejs';
-import all from 'text-image';
+import 'text-image';
 
 const AdminQr = ({restaurant_id}) => {
     const generateQrCode = (event) => {
         event.preventDefault();
+        
         document.getElementById("qr_code").innerHTML = "";
         const location = event.target.location.value;
 
-
-
-        var style = {
+        const style = {
             // Tailwind.css font family
             font: 'Inter var, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
             align: 'center',
@@ -19,9 +18,8 @@ const AdminQr = ({restaurant_id}) => {
             background: 'rgba(255, 255, 255, 255)',
         }
 
-
-        var textImage = window.TextImage(style);
-        var data = textImage.toDataURL(location);
+        const textImage = window.TextImage(style);
+        const data = textImage.toDataURL(location);
 
         const options = {
             text: window.location.host + Routes.restaurant_path(restaurant_id) + "?location=" + location,
@@ -35,25 +33,22 @@ const AdminQr = ({restaurant_id}) => {
         };
 
         new QRCode(document.getElementById("qr_code"), options);
-
-
-
     };
 
     return (
         <div>
-        <form onSubmit={generateQrCode}>
-            <div>
-                <label>Bord Nr:</label>
-            </div>
-            <div>
-                <input name="location" type="text"></input>
-            </div>
-            <div className="py-2">
-                <input className="text-sm bg-gray-200 border border-gray-400 rounded hover:bg-gray-300 py-1 px-2" type="submit" value="Generer"></input>
-            </div>
-        </form>
-        <div id="qr_code"></div>
+            <form onSubmit={generateQrCode}>
+                <div>
+                    <label>Bord Nr:</label>
+                </div>
+                <div>
+                    <input name="location" type="text"></input>
+                </div>
+                <div className="py-2">
+                    <input className="text-sm bg-gray-200 border border-gray-400 rounded hover:bg-gray-300 py-1 px-2" type="submit" value="Generer"></input>
+                </div>
+            </form>
+            <div id="qr_code"></div>
         </div>
     )
 }
