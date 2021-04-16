28ccf6c39312d28171dd47d2d1816d3663ae2045
Mathias Danielsen <mathiasda98@gmail.com>
2021-04-16 10:03:53 +0200

    Fikset at bordnr ble reset hvis du gikk til hovedsiden uten parameteret

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -2,7 +2,9 @@ class RestaurantController < ApplicationController
   skip_before_action :authorize
 
   def show 
-    session[:location] = params[:location]
+    if params[:location] != nil && params[:location] != 0
+      session[:location] = params[:location]
+    end
 
     @restaurant = Restaurant.find(params[:id])
     @allergens = Allergen.all()
