7d4ed21826f9886fa99be14a4d8f97677df1565c
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2021-04-20 10:23:20 +0200

    refactor, more map vs each

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,20 +2,18 @@ class Api::V1::ApiController < ApplicationController
   skip_before_action :authorize
 
   def total_price
-    totalPrice=0
-
-    session[:cart].each do |item|
-      totalPrice+=(MenuItem.find(item["item_id"]).price * item["amount"])
+    total_prices = session[:cart].map do |item|
+      MenuItem.find(item["item_id"]).price * item["amount"]
     end
- 
-    render json: totalPrice 
+
+    render json: total_prices.sum
   end
-  
+
   def total_amount
-    totalAmount=0
-    if session[:cart]!=nil
+    totalAmount = 0
+    if session[:cart] != nil
       session[:cart].each do |item|
-        totalAmount+=(item["amount"])
+        totalAmount += (item["amount"])
       end
     end
     render json: totalAmount
@@ -24,16 +22,14 @@ class Api::V1::ApiController < ApplicationController
   # /api/v1/add_to_cart?menu_item_id=x&amount=y
   def add_to_cart
     session[:cart] ||= []
-    id=params[:menu_item_id].to_i
-    amount=params[:amount].to_i
-    exists=false
-
-
+    id             = params[:menu_item_id].to_i
+    amount         = params[:amount].to_i
+    exists         = false
 
     session[:cart].each do |item|
-      if item["item_id"]==id
-        item["amount"]+=amount
-        exists=true
+      if item["item_id"] == id
+        item["amount"] += amount
+        exists         = true
         break
       end
     end
@@ -41,20 +37,20 @@ class Api::V1::ApiController < ApplicationController
       session[:cart] << { "item_id": id, "amount": amount }
     end
 
-    render json: { 
-      message: 'Successfully added to cart.', 
-    }, status: 200
+    render json: {
+      message: 'Successfully added to cart.',
+    }, status:   200
   end
 
   # /api/v1/add_to_cart?menu_item_id=x
   def add_one_to_cart
-    id=params[:menu_item_id].to_i
-    exists=false
+    id     = params[:menu_item_id].to_i
+    exists = false
 
     session[:cart].each do |item|
-      if item["item_id"]==id
-        item["amount"]+=1
-        exists=true
+      if item["item_id"] == id
+        item["amount"] += 1
+        exists         = true
         break
       end
     end
@@ -62,100 +58,91 @@ class Api::V1::ApiController < ApplicationController
       session[:cart] << { item_id: id, amount: 1 }
     end
     p session[:cart]
- 
+
     render json: { message: 'Successfully added one to cart.' }, status: 200
   end
 
   # /api/v1/add_to_cart?menu_item_id=x
   def remove_one_from_cart
-    id=params[:menu_item_id].to_i
+    id = params[:menu_item_id].to_i
     session[:cart].each do |item|
-      if item["amount"]<=0
+      if item["amount"] <= 0
         session[:cart].delete(item)
       end
 
-      if item["item_id"]==id
-        item["amount"]-=1
+      if item["item_id"] == id
+        item["amount"] -= 1
       end
     end
 
- 
     render json: { message: 'Successfully removed one to cart.' }, status: 200
   end
 
   # /api/v1/add_to_cart?menu_item_id=x
   def remove_all_from_cart
-    id=params[:menu_item_id].to_i
+    id = params[:menu_item_id].to_i
     session[:cart].each do |item|
-      if item["item_id"]==id
+      if item["item_id"] == id
         session[:cart].delete(item)
       end
     end
     p session[:cart]
- 
+
     render json: { message: 'Successfully removed all to cart.' }, status: 200
   end
 
   def empty_cart
     session[:cart] = []
     p session[:cart]
- 
+
     render json: { message: 'Successfully cleared cart.' }, status: 200
   end
 
-
   def get_orders
     restaurant = Restaurant.find(params[:id])
-    
-    orders = []
-
-    restaurant.orders.each do |order|
-      item = {}
-      item[:id] = order.id
-      item[:order_status] = order.order_status
-      item[:customer_info] = order.customer_info
-      item[:location] = order.location
-      item[:created_at] = order.created_at.strftime("%d.%m.%y kl. %H:%M")
-
-
-      orders << item
+    orders     = restaurant.orders.map do |order|
+      {
+        id:            order.id,
+        order_status:  order.order_status,
+        customer_info: order.customer_info,
+        location:      order.location,
+        created_at:    order.created_at.strftime("%d.%m.%y kl. %H:%M")
+      }
     end
-
     render json: orders
   end
 
   def get_order_items
     order_items = []
 
-
     Order.find(params[:id]).order_items.each do |order|
-      order_item = {}
-      order_item[:number] = order.menu_item.number
-      order_item[:name] = order.menu_item.name
+      order_item               = {}
+      order_item[:number]      = order.menu_item.number
+      order_item[:name]        = order.menu_item.name
       order_item[:description] = order.menu_item.description
-      if(order.menu_item.price)
+      if (order.menu_item.price)
         order_item[:total_price] = order.menu_item.price * order.quantity
       else
         order_item[:total_price] = 0
       end
       order_item[:quantity] = order.quantity
-      order_items << order_item 
+      order_items << order_item
     end
 
-    render json: order_items 
+    render json: order_items
   end
 
   def finish_order
-    order = Order.find(params[:id])
+    order              = Order.find(params[:id])
     order.order_status = "Ferdig"
-    order.save()
+    order.save()       # Silent failure :O
 
     render json: { message: 'Successfully finished order.' }, status: 200
   end
 
   def set_item_status
-    item=MenuItem.find(params[:item_id])
-    item.status=params[:status]
+    item        = MenuItem.find(params[:item_id])
+    item.status = params[:status]
     item.save()
 
     render json: { message: 'Successfully changed status.' }, status: 200
