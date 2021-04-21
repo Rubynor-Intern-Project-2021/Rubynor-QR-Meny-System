db3f5902a7c402404c4b262d76eac3a16c49a5d5
jbg92 <jbg92@live.no>
2021-04-21 17:01:27 +0200

    mer av det samme

--- app/javascript/components/modal/modalMenuItem.jsx
+++ app/javascript/components/modal/modalMenuItem.jsx
@@ -24,7 +24,7 @@ const ModalMenuItem = () => {
                     text="Ønsker du å slette denne retten/ drikken?"
                     handleCancel={() => storeModalMenuItem.dispatch(hideModal())}
                     handleConfirm={() => { storeModalMenuItem.dispatch(hideModal());
-                    window.location.href = Routes.admin_set_item_status_path({item_id: itemId, status: 3});}}
+                    window.location.href = Routes.admin_set_item_status_path({item_id: itemId, status: 0});}}
 
                 />
             </div>
