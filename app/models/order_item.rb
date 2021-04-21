b5ecddf1591186fb8d54b1b2ec568ecc7e8b4e2b
nicole bendu <nic.bendu@gmail.com>
2021-04-21 16:15:15 +0200

    enum status for order_items

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,11 @@ class OrderItem < ApplicationRecord
   belongs_to :menu_item
   belongs_to :order
 
+  enum order_item_status: {
+    "Startet" => 0,
+    "Ferdig" => 1
+  }
+
   def price
     menu_item.price
   end
