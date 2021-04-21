ed0d3da42e0ca455056c21341ae65524a75a8375
jbg92 <jbg92@live.no>
2021-04-21 09:56:50 +0200

    forsøk på cart fiks

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -4,6 +4,7 @@ class RestaurantController < ApplicationController
   def show
     if params[:location] != nil && params[:location] != 0
       session[:location] = params[:location]
+      session[:cart]=[]
     end
 
     @restaurant = Restaurant.find(params[:id])
