c3459c57172c77ed5904d5cc7f4b82c5fa76a148
mathias234 <mathiasda98@gmail.com>
2021-04-30 15:07:00 +0200

    Slettet noen debug outputs

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,17 @@
 class OrdersController < ApplicationController
 
   def make_order
-    p "create order 1"
     id = params[:restaurant_id]
     info = params[:customer_info]
     location = params[:location]
 
-    p "create order 2"
     @order = Order.create(restaurant_id: id, order_status: 0, customer_info: info, location: location)
 
-    p "create order 3"
     session[:cart].each do |item|
       @order_item = @order.add_menu_item(item_id: item['item_id'], quantity: item['amount'], order_item_status: 0)
-      p item
     end
 
     session[:cart]=[]
-    p "create order 4"
   end
 
   # GET /orders/1 or /orders/1.json
@@ -27,7 +22,6 @@ class OrdersController < ApplicationController
 
   def emptyCart
     session[:cart] = []
-    p session[:cart]
     redirect_to restaurant_path(params[:restaurant_id])
   end
 
